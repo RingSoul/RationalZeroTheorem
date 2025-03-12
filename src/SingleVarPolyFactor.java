70f7aa283975d9bf58f8ae1d43ea18cb815d6e08
ringsoul <hwh6636@gmail.com>
2025-03-12 16:00:51 -0400

    improve class name and add functionality

---
+++ src/SingleVarPolyFactor.java
@@ -0,0 +1,26 @@
+public record SingleVarPolyFactor(Fraction rationalZero) implements Comparable<SingleVarPolyFactor> {
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    public String toString() {
+        int numerator = rationalZero().numerator();
+        int denominator = rationalZero().denominator();
+        if (numerator == 0) return "x";
+        String sign = (numerator > 0) ? "-" : "+";
+        String coeff = (denominator == 1) ? "" : Integer.toString(denominator);
+        return "(" + coeff + "x " + sign + " " + rationalZero().numerator() + ")";
+    }
+
+    @Override
+    public int compareTo(SingleVarPolyFactor o) {
+        double difference = rationalZero().getValue() - o.rationalZero().getValue();
+        return (int) Math.ceil(difference);
+    }
+}
