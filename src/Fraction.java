7fbbbcd86b7ce6c38a3c16e07b098b3aaa752df5
RingSoul <hwh6636@gmail.com>
2026-05-20 13:46:09 -0400

    feat: update Fraction and MathUtility
    pave the way for synthetic division algorithm

--- src/Fraction.java
+++ src/Fraction.java
@@ -1,8 +1,10 @@
-public class Fraction {
+public final class Fraction extends RZTFactor {
 
     private int numerator;
     private int denominator;
+
     private Fraction(int numerator, int denominator) {
+        super(0);
         this.numerator = numerator;
         this.denominator = denominator;
     }
@@ -10,6 +12,10 @@ public class Fraction {
     public static Fraction with(int numerator, int denominator) {
         if (denominator == 0) throw new IllegalArgumentException("The denominator of a fraction cannot be zero.");
         return new Fraction(numerator, denominator);
+    }
+
+    public static Fraction of(int number) {
+        return new Fraction(number, 1);
     }
 
     public int numerator() {
@@ -21,23 +27,29 @@ public class Fraction {
     }
 
     public Fraction add(Fraction frac) {
-        int newDenominator = (denominator() == frac.denominator()) ?
-                denominator() : denominator() * frac.denominator();
-        int newNumerator = (newDenominator == denominator()) ?
-                numerator() + frac.numerator() :
-                numerator() * frac.denominator() + frac.numerator() * denominator();
-        return new Fraction(newNumerator, newDenominator).simplify();
+        int newNumerator = 0,  newDenominator = 0;
+        if (denominator() == frac.denominator()) {
+            newDenominator = denominator();
+            newNumerator = numerator() +  frac.numerator();
+        }
+        else {
+            newDenominator = denominator() * frac.denominator();
+            newNumerator = numerator() * frac.denominator()
+                    + frac.numerator() * denominator();
+        }
+        return Fraction.with(newNumerator, newDenominator).simplify();
     }
 
-    public Fraction multiply(Fraction scalar) {
-        return new Fraction(
-                scalar.numerator() * numerator(),
-                scalar.denominator() * denominator()
+    public Fraction multiply(Fraction frac) {
+        return Fraction.with(
+                frac.numerator() * numerator(),
+                frac.denominator() * denominator()
         ).simplify();
     }
 
     private Fraction simplify() {
-        return null;
+        int gcd = MathUtility.gcd(numerator(), denominator());
+        return Fraction.with(numerator() / gcd, denominator() / gcd);
     }
 
     public double toDouble() {
--- src/MathUtility.java
+++ src/MathUtility.java
@@ -1,25 +1,73 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* static library */
 public final class MathUtility {
 
+    /**
+     * Euclidean algorithm is used to find the Greatest Common Divisor (GCD) between two numbers
+     * @param a an integer
+     * @param b another integer
+     * @return the positive integer representing the GCD of the two input integers
+     */
     public static int gcd(int a, int b) {
-        return 0;
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
     }
 
+    /**
+     * Return all the factors of a given number
+     * @param num the given number to be divided (factored)
+     * @return a list of numbers that divides the given number
+     */
     public static List<Integer> factorsOf(int num)
     {
         List<Integer> factors = new ArrayList<>();
         factors.add(1);
-        for (int i = 2; i <= num / 2; i++)
-            if (num % i == 0) factors.add(i);
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                factors.add(i);
+                factors.add(num / i);
+            }
+        }
         factors.add(num);
         return factors;
     }
 
-    public static Object syntheticDivision()
+    /**
+     * Perform synthetic division for one-variable polynomial and return result.
+     * @param dividend the polynomial to be divided
+     * @param divisor the "linear term + constant term" polynomial for divisor
+     * @return a 2-tuple that contains the resulting polynomial and the remainder
+     */
+    public static SyntheticDivisionResult syntheticDivision(SingleVarPolynomial dividend, SingleVarLinearFactor divisor)
     {
-        return null;
+        SingleVarPolynomial poly = null;
+        Fraction remainder = null;
+
+
+
+        return new SyntheticDivisionResult(poly, remainder);
+    }
+
+    /**
+     * Store the result of a synthetic division as a 2-tuple.
+     * Remainder's numerator equaling 0 implies divisibility!
+     * @param poly the polynomial that results from the division
+     * @param remainder the remainder (always a number, saved as fraction)
+     */
+    public record SyntheticDivisionResult(SingleVarPolynomial poly, Fraction remainder) {
+        public boolean isDivisible() {
+            return remainder().numerator() == 0;
+        }
     }
 
 }
